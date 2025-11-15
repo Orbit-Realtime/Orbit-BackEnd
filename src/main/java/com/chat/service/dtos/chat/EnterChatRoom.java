e6702bc51ce361a27bbf9d60dcc09c989cede22d
Choi jun hyoek <chlwnsgur1214@naver.com>
2025-11-16 02:48:50 +0900

    기본 생성자 추가

--- src/main/java/com/chat/service/dtos/chat/EnterChatRoom.java
+++ src/main/java/com/chat/service/dtos/chat/EnterChatRoom.java
@@ -2,10 +2,12 @@ package com.chat.service.dtos.chat;
 
 import com.chat.utils.message.BaseWebSocketMessage;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
+@NoArgsConstructor
 public class EnterChatRoom extends BaseWebSocketMessage {
     private Long memberId;
     private Long lastReadChatId;
